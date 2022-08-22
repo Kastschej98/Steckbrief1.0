0804f741f7f5603bad3cd39323a81b5b6573b5e2
konwac <konwac060307@gymnasiumhueck.onmicrosoft.com>
2022-08-22 21:14:59 +0200

    Steckbrief 1.0

---
+++ src/hallo.java
@@ -0,0 +1,22 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        String schrifft = "Steckbrief Koko";
+        String Alter = "Alter : Ich bin 15 Jahre alt.";
+        String hautfarbe = "Hautfarbe : Weiß.";
+        String groeße = "Größe : 186.";
+        String haarfarbe = "Haarfarbe : Braun.";
+        String augenfarbe = "Augenfarbe : Grün.";
+
+        System.out.println( schrifft );
+        System.out.println( Alter );
+        System.out.println(hautfarbe);
+        System.out.println(groeße);
+        System.out.println(augenfarbe);
+        System.out.println(haarfarbe);
+
+
+    }
+}
+
